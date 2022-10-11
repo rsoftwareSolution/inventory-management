18168a7705956dae744d753d18c4c91d810b29b3
Raghvendra <rsoftware2580@gmail.com>
2022-10-11 09:43:17 +0530

    Update in given entit

--- src/main/java/com/inventory/entity/DeliveryChallan.java
+++ src/main/java/com/inventory/entity/DeliveryChallan.java
@@ -26,9 +26,61 @@ import java.io.Serializable;
 @AllArgsConstructor
 @NoArgsConstructor
 @Table(name = "DELIVERY_CHALLAN")
-public class DeliveryChallan {
+public class DeliveryChallan implements Serializable {
+
+    private static final long serialVersionUID = 3814023133819751531L;
 
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "DELIVERY_CHALLAN_ID")
     private Long deliveryChallanId;
+
+    @Column(name = "PRODUCT_ID")
+    private Long productId;//for getting delivery product details
+
+    @Column(name = "CUSTOMER_ID")
+    private Long customerId;//getting customer details
+
+    @Column(name = "COMPANY_ID")
+    private Long companyId;//getting company details like name, address, phone, GSTIN
+
+    @Column(name = "ORDER_DATE")
+    private String orderDate;
+
+    @Column(name = "DELIVERY_DATE")
+    private String deliveryDate;
+
+    @Column(name = "DISPATCH_DATE")
+    private String dispatchDate;
+
+    @Column(name = "PLACE_OF_SUPPLY")
+    private String placeOfSupply;
+
+    @Column(name = "QUANTITY")
+    private String quantity;
+
+    @Column(name = "TAXABLE_VALUE")
+    private String taxableValue;
+
+    @Column(name = "CGST")
+    private Double cgst;
+
+    @Column(name = "SGST")
+    private Double sgst;
+
+    @Column(name = "IGST")
+    private Double igst;
+
+    @Column(name = "SUB_TOTAL")
+    private Double subTotal;
+
+    @Column(name = "TOTAL_TAX")
+    private Double totalTax;
+
+    @Column(name = "ROUND_OFF")
+    private Double roundOff;
+
+    @Column(name = "GRAND_TOTAL")
+    private String grandTotal;
 
 }
